7cf8fa612e0bb1edd8855ca5895226dacc51d4cc
wootaepark <pakdal99@gmail.com>
2024-02-01 11:02:02 +0900

    240201_1_comit

---
+++ ch2/ex1.js
@@ -0,0 +1,14 @@
+if(true){
+    var x= 3;
+}
+console.log(x);
+
+if(true){
+    const y = 3;
+
+}
+console.log(y); // error 발생 
+
+
+// var 은 함수 스코프라서 외부에서 사용가능
+// const, let 은 블록스코프 이므로 블록 밖에서는 사용 불가능 
