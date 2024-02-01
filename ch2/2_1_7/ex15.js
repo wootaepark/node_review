cab631dc3915920fa5c88fc3acbbb5283dad8d39
wootaepark <pakdal99@gmail.com>
2024-02-01 17:28:57 +0900

    240201_4_commit

---
+++ ch2/2_1_7/ex15.js
@@ -0,0 +1,23 @@
+const condition = true;
+const promise = new Promise((resolve, reject)=>{
+    if(condition){
+        resolve('성공');
+    }
+    else{
+        reject('실패');
+    }
+});
+// 다른 코드 삽입 가능 
+
+promise
+    .then((message)=>{
+        console.log(message);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+    .finally(()=>{
+        console.log('무조건');
+    })
+
+// ex15 : 프로미스 
---
+++ ch2/2_1_7/ex16.js
@@ -0,0 +1,29 @@
+const condition = true;
+const promise = new Promise((resolve, reject)=>{
+    if(condition){
+        resolve('성공');
+    }
+    else{
+        reject('실패');
+    }
+});
+
+promise
+    .then((message)=>{
+        return new Promise((resolve, reject)=>{
+            resolve(message);
+        });
+    })
+    .then((message2)=>{
+        return new Promise((resolve, reject)=>{
+            resolve(message2);
+        });
+    })
+    .then((message3)=>{
+        console.log(message3);
+    })
+    .catch((error)=>{
+        console.error(error);
+    })
+
+    // ex16 : 프로미스 리턴을 통해 다음 then 문으로 이동시키는 코드 
