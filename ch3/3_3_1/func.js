e63a30066ed2fe353600d7220467f7d028aee7fa
wootaepark <pakdal99@gmail.com>
2024-02-03 18:29:18 +0900

    240203_commit

--- ch3/3_3_1/func.js
+++ ch3/3_3_1/func.js
@@ -7,7 +7,7 @@ function checkOddOrEven(num){
     return even;
 }
 
-module.exports = checkOddOrEven; // 이 경우는 해당 함수만을 export
+module.exports = checkOddOrEven; // 이 경우는 해당 함수만을 export 하는 개념으로 알아두자 
 
 // exports.checkOddOrEven = checkOddOrEven; 으로 쓸 수 있다.
 // index.js 에서는 해당 모듈을 불러온다 .(예를 들어 check 라는 이름으로)
