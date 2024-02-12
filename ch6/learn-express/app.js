b4f8d4323e2857ab5f7214e2448a19ac16701684
wootaepark <pakdal99@gmail.com>
2024-02-12 17:03:56 +0900

    240212_2_commit

--- ch6/learn-express/app.js
+++ ch6/learn-express/app.js
@@ -1,16 +1,82 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
+dotenv.config(); // process.env를 사용할 수 있도록 하는 명령어
 const app = express();
-
 app.set('port', process.env.PORT || 3000);
 
-app.get('/',(req,res)=>{
-    //res.send('Hello Express');
-    res.sendFile(path.join(__dirname,'index.html'));
-    // '/index.html' 과 'index.html'의 차이점 : (index라는 이름이라 다른것이지 다른 이름이면 같은 역할을 한다.)
-    // 슬래시가 있으면 루트 디렉토리에서 시작되도록 하며 절대 경로가 되지만
-    // 없으면 현재 디렉토리에서의 상대 경로로 간주된다. (by chat-gpt);
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser(process.env.COOKIE_SECRET)); // 쿠키 서명을 위한 비밀 키를 설정한다. (.env 파일에 따로 설정)
+app.use(session({
+    resave : false,
+    saveUninitialized : false,
+    secret : process.env.COOKIE_SECRET,
+    cookie : {
+        httpOnly : true,
+        secure : false,
+    },name : 'session-cookie',
+}));
+
+
+const multer = require('multer');
+const fs = require('fs');
+
+try{
+    fs.readdirSync('uploads');
+}catch(error){
+    console.error('uploads 폴더가 없어서 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage : multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname,ext) + Date.now() + ext);
+        },
+    }),
+    limits : {fileSize : 5*1024*1024},
+});
+
+app.get('/upload', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+app.post('/upload', upload.fields([{name : 'image1'},{name:'image2'}]), (req,res)=>{
+    console.log(req.file);
+    res.send('Ok');
+})
+
+
+
+
+
+app.use((req,res,next)=>{
+
+    console.log('모든 요청에 다 실행됩니다');
+    next();
+});
+
+app.get('/',(req,res,next)=>{
+
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
+    
+},(req, res)=>{
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+});
+
+app.use((err, req, res, next)=>{
+    console.error(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'),()=>{
