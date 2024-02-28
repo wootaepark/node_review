c2c1ed879081902051fc6b806734b410631e63b8
wootaepark <pakdal99@gmail.com>
2024-02-28 15:42:19 +0900

    240228_1_commit

---
+++ ch8/learn-mongoose/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const nunjucks = require('nunjucks');
+const morgan = require('morgan');
+
+const connect = require('./schemas');
+
+const app = express();
+app.set('port', process.env.PORT || 3002);
+app.set('view engine', 'html');
+nunjucks.configure('views',{
+    express : app,
+    watch : true,
+});
+
+connect();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.use((req, res, next)=>{
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((req, res, next)=>{
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
+
+
+
+
+
