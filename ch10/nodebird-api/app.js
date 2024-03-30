146529935306f959682416a3e99438d660116e9d
wootaepark <pakdal99@gmail.com>
2024-03-30 18:10:14 +0900

    temp-commit-2

--- ch10/nodebird-api/app.js
+++ ch10/nodebird-api/app.js
@@ -75,5 +75,5 @@ app.use((err, req, res, next)=>{
 
 app.listen(app.get('port'),()=>{
     console.log(app.get('port'), '번 포트에서 대기 중');
-    console.log(app.get('view engine'));
+    //console.log(app.get('view engine'));
 })
